f806f16cf3f12afd5f1f51a3ad655760e2f4c59c
sahooabinash2003@gmail.com <avinashsahoo2021@gmail.com>
2025-06-24 23:45:30 +0530

    server modification

--- server/src/index.ts
+++ server/src/index.ts
@@ -33,7 +33,7 @@ app.use("/", (req, res) => {
 });
 
 // SERVER SETUP
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const port = Number(process.env.PORT) || 3000;
+app.listen(port, "0.0.0.0", () => {
   console.log(`Server is running on port : ${port}`);
 });
